f718200cf8aca454a1d4e3cbae94b228bd96cb7e
GabrielBarcelos1 <gabrielvieirabarcelos02@gmail.com>
2024-02-26 20:11:10 +0000

    add .

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ require('dotenv').config();
 
 app.use(cors());
 
-// Middleware para permitir CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'x-prolog-api-token, Content-Type, Accept');
-  next();
-});
-
 // Rota para encaminhar todas as solicitações GET
 app.get('/api/*', async (req, res) => {
 const path = req.params[0]
@@ -32,5 +24,5 @@ console.log(`console.log https://prologapp.com/prolog/api/v3/${path}`)
   }
 });
 
-const PORT = 3002;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
